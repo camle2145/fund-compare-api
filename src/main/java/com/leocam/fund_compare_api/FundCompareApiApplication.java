e7707d8dbfc6a0d17f64830731854fb988e25df3
Leo Cameron <leocam2145@gmail.com>
2025-12-23 18:34:01 +1300

    Added Spring Boot files

---
+++ src/main/java/com/leocam/fund_compare_api/FundCompareApiApplication.java
@@ -0,0 +1,13 @@
+package com.leocam.fund_compare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundCompareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundCompareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leocam/fund_compare_api/FundCompareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leocam.fund_compare_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundCompareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
